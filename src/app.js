ed772b1a43b83c89a77ee1f104f6acbf7132cf69
christhecaribou <dizzyquill@protonmail.com>
2020-08-20 19:28:57 -0700

    No functional changes; cleaner formatting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 var express = require('express');
-var app = express();app.get('/', function (req, res) {
+var app = express();
+
+console.log('Beginning app.js ...');
+
+app.get('/', function (req, res) {
   res.send('Hello World!');
-});app.listen(3000, function () {
+});
+
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
